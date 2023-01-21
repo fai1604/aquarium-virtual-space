00a936046c296a37446db4792d6f6ae36b2e5c3b
Mido <mohammad.elgilani@gmail.com>
2023-01-21 16:05:57 +0800

    UndoBuild_Phase2 -build Undo Using Command

--- src/main/java/org/aquarium/Controls.java
+++ src/main/java/org/aquarium/Controls.java
@@ -4,15 +4,12 @@ import java.util.Stack;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
-import javafx.stage.Popup;
+
 
 public class Controls extends TilePane {
     public Button[] entityButtons = new Button[5];
@@ -21,6 +18,8 @@ public class Controls extends TilePane {
     private Stack<Button> stackBtn = new Stack<>();
     private Stack<Integer> stackInt = new Stack<>();
     private Stack<String> stackStr = new Stack<>();
+    private Toggle toggle = new Toggle();
+    private ToggleEntityVisibility toggleV = new ToggleEntityVisibility(toggle);
 
     public Controls(Space space) {
         this.space = space;
@@ -49,19 +48,6 @@ public class Controls extends TilePane {
             this.setAlignment(Pos.CENTER);
         }
 
-        // Stack<ToggleEntityVisibilityCommand> stack = new Stack<>();
-        // for (int i = 0; i < entityButtonNames.length; i++) {
-        //     final Integer innerIndex = i;
-        //     entityButtons[innerIndex] = new Button("Add " + entityButtonNames[innerIndex]);
-        //     entityButtons[innerIndex].setMinSize(120, 20);
-        //     entityButtons[innerIndex].setPadding(new Insets(10));
-        //     entityButtons[innerIndex].setOnAction(e -> {
-        //         ToggleEntityVisibilityCommand cmd = new ToggleEntityVisibilityCommand(stack, entityButtons[innerIndex], innerIndex, entityButtonNames[innerIndex]);
-        //         cmd.execute();
-        //     });
-        //     this.getChildren().add(entityButtons[innerIndex]);
-        //     this.setAlignment(Pos.CENTER);
-        // }
 
         Button badgeBtn = new Button("See Badges");
         badgeBtn.setMinSize(120, 20);
@@ -90,7 +76,8 @@ public class Controls extends TilePane {
         undoBtn.setMinSize(120, 20);
         undoBtn.setPadding(new Insets(10));
         undoBtn.setOnAction(e -> {
-            UndotoggleEntityVisibility();
+            // UndotoggleEntityVisibility();
+            toggleV.undo();
         });
         this.getChildren().add(undoBtn);
 
@@ -107,42 +94,58 @@ public class Controls extends TilePane {
         this.setVgap(20);
     }
 
-    public void toggleEntityVisibility(Button button, int index, String entityName) {
-        stackBtn.push(button);
-        stackInt.push(index);
-        stackStr.push(entityName);
-        boolean visibility = space.toggleEntity(index);
-        if (!visibility) {
-            button.setText("Add " + entityName);
-        } else {
-            button.setText("Remove " + entityName);
-        }
+    public void toggleEntityVisibility( Button button1, int index1, String entityName1) {
+        
+        toggle.setSpace(space);
+        toggle.setStackBtn(stackBtn);
+        toggle.setStackInt(stackInt);
+        toggle.setStackStr(stackStr);
+        toggle.setButton(button1);
+        toggle.setEntityName(entityName1);
+        toggle.setIndex(index1);
+        toggleV.execute();
     }
-    
-    public void UndotoggleEntityVisibility() {
-        if (!stackBtn.isEmpty()){
-            
-            Button button = stackBtn.pop();
-            Integer index = stackInt.pop();
-            String entityName = stackStr.pop();
 
-            // String next;
-            // while (!stackBtn.empty()) {
-            //     next = stackStr.peek();
-            //     if (entityName.equals(next)) {
-            //         stackBtn.remove(stackBtn.size()-1);
-            //         stackInt.remove(stackInt.size()-1);
-            //         stackStr.remove(stackStr.size()-1);
-            //     }
-            // }
-            boolean visibility = space.toggleEntity(index);
-            if (!visibility) {
-                button.setText("Add " + entityName);
-            } else {
-                button.setText("Remove " + entityName);
-            }
-        }
-    }
+    // public void toggleEntityVisibility( Button button, int index, String entityName) {
+
+        // stackBtn.push(button);
+        // stackInt.push(index);
+        // stackStr.push(entityName);
+
+        // boolean visibility = space.toggleEntity(index);
+        // if (!visibility) {
+        //     button.setText("Add " + entityName);
+        // } else {
+        //     button.setText("Remove " + entityName);
+        // }
+
+        
+    // }
+    
+    // public void UndotoggleEntityVisibility() {
+    //     if (!stackBtn.isEmpty()){
+            
+    //         Button button = stackBtn.pop();
+    //         Integer index = stackInt.pop();
+    //         String entityName = stackStr.pop();
+
+    //         // String next;
+    //         // while (!stackBtn.empty()) {
+    //         //     next = stackStr.peek();
+    //         //     if (entityName.equals(next)) {
+    //         //         stackBtn.remove(stackBtn.size()-1);
+    //         //         stackInt.remove(stackInt.size()-1);
+    //         //         stackStr.remove(stackStr.size()-1);
+    //         //     }
+    //         // }
+    //         boolean visibility = space.toggleEntity(index);
+    //         if (!visibility) {
+    //             button.setText("Add " + entityName);
+    //         } else {
+    //             button.setText("Remove " + entityName);
+    //         }
+    //     }
+    // }
 
     public void toggleBadgeListVisibility(Button button) {
         boolean visibility = space.toggleBadgeListPopup();
