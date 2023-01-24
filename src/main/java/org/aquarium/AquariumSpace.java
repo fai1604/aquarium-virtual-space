f0737742bab657269fe79c4f561b8f9697c63348
Mido <mohammad.elgilani@gmail.com>
2023-01-24 12:14:34 +0800

    Updates - add Logo to Interface      -neonFish.png - set interface size to full screen

--- src/main/java/org/aquarium/AquariumSpace.java
+++ src/main/java/org/aquarium/AquariumSpace.java
@@ -3,6 +3,7 @@ package org.aquarium;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,6 +13,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Popup;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.Media;
@@ -33,13 +35,23 @@ public class AquariumSpace {
         BorderPane root = new BorderPane();
         Space space = new Space(this);
         Controls controls = new Controls(space);
-
         root.setTop(space);
         root.setBottom(controls);
 
-        Scene scene = new Scene(root, 1400, 780);
+        // set to full screen at start
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setFullScreen(true);
+        stage.setHeight(screenBounds.getHeight());
+        stage.setWidth(screenBounds.getWidth());
+
+        Scene scene = new Scene(root, stage.getWidth(), stage.getHeight());
+        //set application icon
+        Image image = new Image(getClass().getResourceAsStream("/org/aquarium/images/NeonFish.png"));
+        stage.getIcons().add(image);
 
         stage.setTitle("Aquarium Space");
+
+        stage.setResizable(true);
         stage.setScene(scene);
         stage.show();
     }
